e0d1bed4179d4df1185d2e78227d5643a09ff24f
cadegenn <cadegenn@univ-lr.fr>
2015-01-08 08:45:17 +0100

    fix command line argument list

--- wolpd.c
+++ wolpd.c
@@ -111,7 +111,7 @@ void parse_options(int argc, char *argv[])
             {NULL, 0, NULL, 0}
         };
 
-        if ((c = getopt_long(argc, argv, "hvi:f",
+        if ((c = getopt_long(argc, argv, "hvifpd",
                      long_options, &option_index)) == -1) break;
 
         switch (c) {
